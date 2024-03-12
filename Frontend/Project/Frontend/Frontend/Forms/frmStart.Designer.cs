e78ebb1a29808818c740db9918c623bfb99103b7
Emrah Veladžić <148920444+EmrahVeladzic@users.noreply.github.com>
2024-03-12 11:39:20 +0100

    Basic

--- Frontend/Project/Frontend/Frontend/Forms/frmStart.Designer.cs
+++ Frontend/Project/Frontend/Frontend/Forms/frmStart.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace Frontend.Forms
+﻿using System.Runtime.Versioning;
+
+
+namespace Frontend.Forms
 {
     partial class frmStart
     {
@@ -28,19 +31,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmStart));
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(230, 426);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(25, 16);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(176, 174);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.Location = new Point(248, 12);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(540, 426);
+            panel2.TabIndex = 1;
             // 
             // frmStart
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
             ClientSize = new Size(800, 450);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Name = "frmStart";
-            Text = "frmStart";
+            Text = "Garaža Makerspace";
             Load += frmStart_Load;
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Panel panel2;
     }
 }
--- Frontend/Project/Frontend/Frontend/obj/Debug/net8.0-windows/Frontend.AssemblyInfo.cs
+++ Frontend/Project/Frontend/Frontend/obj/Debug/net8.0-windows/Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89559608cf14cb28adaadf117b4621ff2bbe7ee2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+805fa0bc772167f45ddbeefb5228898860261882")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
